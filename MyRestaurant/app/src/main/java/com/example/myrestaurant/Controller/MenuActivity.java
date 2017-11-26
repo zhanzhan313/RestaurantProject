4f5643d7f8e88461cdef256d22f4c11018676488
Ruoran Wang <wang.ruor@husky.neu.edu>
2017-11-26 12:22:59 -0500

    add menu and refine manifest.xml

--- MyRestaurant/app/src/main/java/com/example/myrestaurant/Controller/MenuActivity.java
+++ MyRestaurant/app/src/main/java/com/example/myrestaurant/Controller/MenuActivity.java
@@ -1,12 +1,14 @@
 package com.example.myrestaurant.Controller;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.myrestaurant.R;
 
@@ -22,4 +24,27 @@ public class MenuActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu,menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.orderhistory:
+                Toast.makeText(this,"Navigate to order page",Toast.LENGTH_SHORT).show();
+//                Intent intent= new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+                break;
+            case R.id.orderfood:
+                Toast.makeText(this,"Navigate to order history page",Toast.LENGTH_SHORT).show();
+
+        }
+        return  true;
+    }
 }
