84ad4b345616759518d7bfe430d7b6e6a901419a
Vijayalakshmi S <vj@Vijayalakshmis-MacBook-Pro.local>
2017-11-30 21:21:02 -0500

    Passing Order object b/w UI->Server

--- MyRestaurant/app/src/main/java/com/example/myrestaurant/Model/Order.java
+++ MyRestaurant/app/src/main/java/com/example/myrestaurant/Model/Order.java
@@ -3,36 +3,77 @@ package com.example.myrestaurant.Model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.example.myrestaurant.Controller.SignupLogin;
-
-import java.util.*;
-
 public class Order implements Parcelable {
     // In this class actionTodo = Orderplacement
     private String actionTodo;
-    private String orderPlacedTime;
+    private double orderPlacedTime;
     private String userName;
-    private static int orderID=0;
-    /*
-   orderItemQuantity[0] - Indicates quantity for Burger.
-   orderItemQuantity[1] - Indicates quantity for Chicken.
-   orderItemQuantity[2] - Indicates quantity for FrenchFries.
-   orderItemQuantity[3] - Indicates quantity for OnionRings.
-   */
+    private static int orderID = 0;
+
     private int[] orderItemQuantity = new int[4];
     private double orderTotal;
 
-    public Order() {
-        orderID++;
+    public Order(String userName, int[] orderItemQuantity) {
+        this.userName = userName;
+        this.orderItemQuantity = orderItemQuantity;
     }
+
+    protected Order(Parcel in) {
+        actionTodo = in.readString();
+        orderPlacedTime = in.readDouble();
+        userName = in.readString();
+        orderItemQuantity = in.createIntArray();
+        orderTotal = in.readDouble();
+    }
+
+    public static final Creator<Order> CREATOR = new Creator<Order>() {
+        @Override
+        public Order createFromParcel(Parcel in) {
+            return new Order(in);
+        }
+
+        @Override
+        public Order[] newArray(int size) {
+            return new Order[size];
+        }
+    };
+
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(actionTodo);
-        dest.writeString(orderPlacedTime);
-        dest.writeString(userName);
-        dest.writeInt(orderID);
-        dest.writeIntArray(orderItemQuantity);
-        dest.writeDouble(orderTotal);
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(actionTodo);
+        parcel.writeDouble(orderPlacedTime);
+        parcel.writeString(userName);
+        parcel.writeIntArray(orderItemQuantity);
+        parcel.writeDouble(orderTotal);
+    }
+
+    public String getActionTodo() {
+        return actionTodo;
+    }
+
+    public void setActionTodo(String actionTodo) {
+        this.actionTodo = actionTodo;
+    }
+
+    public double getOrderPlacedTime() {
+        return orderPlacedTime;
+    }
+
+    public void setOrderPlacedTime(double orderPlacedTime) {
+        this.orderPlacedTime = orderPlacedTime;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public static int getOrderID() {
@@ -41,6 +82,10 @@ public class Order implements Parcelable {
 
     public static void setOrderID(int orderID) {
         Order.orderID = orderID;
+    }
+
+    public int[] getOrderItemQuantity() {
+        return orderItemQuantity;
     }
 
     public void setOrderItemQuantity(int[] orderItemQuantity) {
@@ -54,62 +99,4 @@ public class Order implements Parcelable {
     public void setOrderTotal(double orderTotal) {
         this.orderTotal = orderTotal;
     }
-
-    public static Creator<Order> getCREATOR() {
-        return CREATOR;
-    }
-
-    public static final Creator<Order> CREATOR = new Creator<Order>() {
-        @Override
-        public Order createFromParcel(Parcel source) {
-            Order order=new Order();
-            order.setActionTodo(source.readString());
-            order.setOrderPlacedTime(source.readString());
-            order.setUserName(source.readString());
-            order.setOrderID(source.readInt());
-//            order.setOrderItemQuantity(source.readIntArray());
-            order.setOrderTotal(source.readFloat());
-            return order;
-        }
-
-        @Override
-        public Order[] newArray(int size) {
-            return new Order[0];
-        }
-    };
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-
-    public String getActionTodo() {
-        return actionTodo;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-
-    public void setActionTodo(String actionTodo) {
-        this.actionTodo = actionTodo;
-    }
-
-    public String getOrderPlacedTime() {
-        return orderPlacedTime;
-    }
-
-    public void setOrderPlacedTime(String orderPlacedTime) {
-        this.orderPlacedTime = orderPlacedTime;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int[] getOrderItemQuantity() {
-        return orderItemQuantity;
-    }
-
 }
